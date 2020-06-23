51e49140e625962d8c0f26c276f1e1b364bb970c
Musango Wope <musangow@gmail.com>
2020-06-24 00:38:53 +0200

    Create HelpContent.js

---
+++ src/components/Quiz/HelpContent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import themed from '../../functions/themed';
+import { createMarkup } from '../../functions/createMarkup.func';
+
+const HelpContainer = styled.div`
+  background-color: ${(props) => props.theme.colors.secondary};
+  padding: 20px;
+`;
+
+const HelpTitle = styled.div`
+  font-size: ${(props) => props.theme.fontSizes[3]};
+  //font-weight: bold;
+  color: ${(props) => props.theme.colors.white};
+  border-bottom: 1px solid ${(props) => props.theme.colors.white};
+  margin-bottom: 15px;
+`;
+
+const HelpBody = styled.div`
+  color: white;
+`;
+
+const HelpContent = ({ content, title }) => {
+  return (
+    <HelpContainer>
+      <HelpTitle>{title}</HelpTitle>
+      <HelpBody dangerouslySetInnerHTML={createMarkup(content)} />
+    </HelpContainer>
+  );
+};
+
+HelpContent.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.any,
+};
+HelpContent.defaultProps = {
+  title: '',
+  content: null,
+};
+
+export default themed(HelpContent);
