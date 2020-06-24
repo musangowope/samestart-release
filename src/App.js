ebb1ee3df7cb4191af65cb4ba8baa344e0ee47bf
Musango Wope <musangow@gmail.com>
2020-06-24 23:56:28 +0200

    remove QuizView/js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { Router } from '@reach/router';
 import HomeView from './views/HomeView';
 import SubjectsView from './views/SubjectsView';
 import LessonView from './views/LessonView';
-import QuizView from './views/QuizView';
 import SSNavbar from './components/SSNavbar';
 import SyllabusView from './views/SyllabusView';
 import MobileNavbar from './components/SSNavbar/MobileNavbar';
 import YenzaView from './views/YenzaView';
 
-const App = (props) => (
+const App = () => (
   <React.Fragment>
     <SSNavbar />
     <Router>
@@ -17,7 +16,6 @@ const App = (props) => (
       <SyllabusView path="syllabus" />
       <SubjectsView path="subjects" />
       <LessonView path="lesson" />
-      <QuizView path="quiz" />
       <YenzaView path="yenza" />
     </Router>
     <MobileNavbar />
