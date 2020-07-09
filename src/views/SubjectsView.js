3ed8c12bccd8a206cc785beea9fc6762727e43ca
Musango Wope <musangow@gmail.com>
2020-07-09 18:51:45 +0200

    SubjectsView.js: remove yenza heading

--- src/views/SubjectsView.js
+++ src/views/SubjectsView.js
@@ -4,41 +4,7 @@ import SubjectAccordion from '../components/SubjectAccordion';
 import axios from 'axios';
 import api from 'constants/api';
 import { baseRequestState } from '../constants/baseRequest';
-import styled from 'styled-components';
-import YenzaImg from 'images/yenza-background.jpg';
 import themed from '../functions/themed';
-
-const YenzaLink = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: transparent;
-  border: 2px solid ${(props) => props.theme.colors.tertiary};
-  background-image: url("${YenzaImg}");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  box-shadow: none;
-  height: 100px !important;
-  width: 384px;
-  padding: 20px;
-  color: white;
-  font-size: ${(props) => props.theme.fontSizes[3]};
-  &:hover {
-    color: ${(props) => props.theme.colors.white};
-  }
-  @media screen and (max-width: ${(props) =>
-    props.theme.breakpoints.md}) {
-    width: 100%;
-    text-align: center;
-  }
-`;
-
-const YenzaHeading = styled.div`
-  font-size: ${(props) => props.theme.fontSizes[4]};
-  color: ${(props) => props.theme.colors.base};
-  margin-bottom: 20px;
-`;
 
 const SubjectsView = () => {
   const [request, setRequest] = React.useState(baseRequestState);
