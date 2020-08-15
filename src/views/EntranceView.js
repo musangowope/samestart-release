d9643a4ee75adbe7eceece21c7e68c22dd7e03a1
Musango Wope <musangow@gmail.com>
2020-08-15 13:22:14 +0200

    create EntranceView

---
+++ src/views/EntranceView.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import GenericSection from '../components/GenericSection';
+import ShapedBackground from '../components/ShapedBackground';
+import themed from '../functions/themed';
+import CareerGuidanceSrc from '../svgs/career-guidance.svg';
+import MultilingualEduSrc from '../svgs/multilingual-icon.svg';
+import ActionCardLink from '../components/ActionCardLink';
+import serviceConstants from '../constants/serviceConstants';
+
+const EntranceView = () => {
+  return (
+    <ShapedBackground>
+      <GenericSection title="Start your learning journey">
+        <div className="mb-5">
+          <ActionCardLink
+            to={`/service?name=${serviceConstants.YENZA_SERVICE}`}
+            cardTitle="Career Guidance"
+            cardBody="Smart Career Technology will put you on a path towards
+              career success."
+            svgSrc={CareerGuidanceSrc}
+          />
+        </div>
+        <ActionCardLink
+          to={`/subjects`}
+          cardTitle="Multilingual Education"
+          cardBody="Coming Soon"
+          svgSrc={MultilingualEduSrc}
+          disabled={true}
+        />
+      </GenericSection>
+    </ShapedBackground>
+  );
+};
+
+EntranceView.propTypes = {};
+EntranceView.defaultProps = {};
+
+export default themed(EntranceView);
