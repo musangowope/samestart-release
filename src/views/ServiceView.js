b82d7795b702ae94be23e5c9d6e6c6279e3a5b1b
Musango Wope <musangow@gmail.com>
2020-12-03 18:23:00 +0200

    ServiceView.js: comment out exit button

--- src/views/ServiceView.js
+++ src/views/ServiceView.js
@@ -87,10 +87,10 @@ const ServiceView = (props) => {
         <S.IframeContainer vpHeight={vpHeight - 50}>
           {getIframe()}
         </S.IframeContainer>
-        <S.ExitButton onClick={() => navigate('/')}>
-          <span>Exit</span>
-          <InlineSVG src={LogoSrc} />
-        </S.ExitButton>
+        {/*<S.ExitButton onClick={() => navigate('/')}>*/}
+        {/*  <span>Exit</span>*/}
+        {/*  <InlineSVG src={LogoSrc} />*/}
+        {/*</S.ExitButton>*/}
       </S.ServiceContainer>
     </React.Fragment>
   );
