2adcc722abddd32b6cc545ffe7cd3368d0413841
Musango Wope <musangow@gmail.com>
2021-01-06 19:49:16 +0200

    show url

--- src/views/EntranceView.js
+++ src/views/EntranceView.js
@@ -12,6 +12,7 @@ const EntranceView = () => {
     <ShapedBackground>
       <GenericSection title="Begin your journey">
         <div className="mb-5">
+          <div>{window.location.href}</div>
           <ActionCardLink
             to={`/service?name=${serviceConstants.YENZA_SERVICE}`}
             cardTitle="Career"
