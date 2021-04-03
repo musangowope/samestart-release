77716e7f6cc440b981ec5fc9b3c7ef3ef4fdcfbe
Musango Wope <musangow@gmail.com>
2021-04-03 17:31:45 +0200

    SimpleModal.js: remove unused css import

--- src/components/SimpleModal.js
+++ src/components/SimpleModal.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { handleOutsideElementClick } from 'functions/handleOutsideElementClick.func';
-import styled, { css } from 'styled-components';
+import styled  from 'styled-components';
 import { transparentize } from 'polished';
 import themed from 'functions/themed';
 import ReactDOM from 'react-dom';
