933c130660b46fceb2ac11bef652b269cce17257
Musango Wope <musangow@gmail.com>
2021-04-05 12:19:04 +0200

    src/index.js: comment out unused provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import 'css/styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+// import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
