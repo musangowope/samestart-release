6c712949a85c8982178af746523ee20e0b46af63
Musango <musangow@gmail.com>
2022-01-17 23:25:22 +0200

    create sample data and update api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,24 +1,23 @@
 import { hasValue } from '../functions/hasValue.func';
 
 export default {
-  getQuizByLessonId: (id = '') =>
-    hasValue(id)
-      ? `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/quiz/${id}`
-      : '',
-  getSubjects: () =>
-    `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/subjects`,
-  getSyllabus: (courseId = '') =>
-    hasValue(courseId)
-      ? `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/syllabus/${courseId}`
-      : '',
-  getLesson: (lessonId = '') =>
-    hasValue(lessonId)
-      ? `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/lesson/${lessonId}`
-      : '',
-  searchByAwezaTermByString: (term = '') =>
-    `${process.env.REACT_APP_AWEZA_ENDPOINT}/terms/search/${term}`,
-  searchByAwezaTermById: (id = '') =>
-    hasValue(id)
-      ? `${process.env.REACT_APP_AWEZA_ENDPOINT}/terms/${id}`
-      : '',
+  getQuizByLessonId: (id = '') => {
+    // ${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/quiz/${id}
+    return hasValue(id) ? `/sample-data/quiz-${id}.json` : '';
+  },
+  getSubjects: () => {
+    // `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/subjects`
+    return '/sample-data/subjects.json';
+  },
+
+  getSyllabus: (courseId = '') => {
+    // `${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/syllabus/${courseId}`
+    return '/sample-data/syllabus.json';
+  },
+  getLesson: (lessonId = '') => {
+    // ${process.env.REACT_APP_WP_ENDPOINT}/wp-json/samestart-lms/v1/lesson/${lessonId}`
+    return hasValue(lessonId)
+      ? `/sample-data/lesson-${lessonId}.json`
+      : '';
+  },
 };
